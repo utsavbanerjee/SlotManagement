05ad8ab286f0ced813daa8f612f602df137d77a6
Utsav Banerjee <utsav.banerjee@camycent.com>
2015-04-16 22:00:14 +0530

    Project Init

---
+++ SlotManagement/SlotManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlotManagement.Startup))]
+namespace SlotManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
